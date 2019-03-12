16bc1c09d4fcc50f34f16884c541780b1f92d45e
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 18:57:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,18 @@ def badge_maker(name)
 end
 
 
+def batch_badge_creator(names)
+  
+end
+
+
+def assign_rooms(name, room)
+  counter = 0 
+
+  assign_rooms
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
+end
